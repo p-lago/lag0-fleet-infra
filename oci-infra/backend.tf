26110211b2f26b00e73d59a47eb144325b783e9f
antoniolago <45375617+antoniolago@users.noreply.github.com>
2023-11-18 16:57:27 -0300

    Node pool increase

---
+++ oci-infra/backend.tf
@@ -0,0 +1,6 @@
+terraform {
+    backend "http" {
+        address = "https://objectstorage.sa-saopaulo-1.oraclecloud.com/p/E_SfVyERw9Xm0yhEPz9BsmsMRksQ_cdMUcEKr1mzvYblDyPjAYlWlOv_2LqWw2IR/n/grkwgmqwtyw9/b/bucket-20231001-2114/o/state"
+        update_method = "PUT"
+    }
+}
